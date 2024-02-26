03b53f5e286cf2de2a53e55a18a67627c2aa34da
ymehili <youssefmehili@gmail.com>
2024-02-26 22:25:54 +0100

    Refactor item image fetching and display

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-async function getItemImage(itemId) {
-    const response = await fetch(`https://api.kedufront.juniortaker.com/item/picture/${itemId}`);
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    // Assuming the image is returned as a blob
-    const imageBlob = await response.blob();
-
-    // Create an object URL for the image
-    const imageUrl = URL.createObjectURL(imageBlob);
-
-    return imageUrl;
-}
-
 fetch('https://api.kedufront.juniortaker.com/item/')
     .then(response => response.json())
     .then(data => {
@@ -22,12 +6,14 @@ fetch('https://api.kedufront.juniortaker.com/item/')
             const itemElement = document.createElement('div');
             itemElement.classList.add('item-box');
 
+            id = item._id;
             const nameElement = document.createElement('p');
             nameElement.textContent = item.name;
             nameElement.classList.add('item-name');
             itemElement.appendChild(nameElement);
 
             const imageElement = document.createElement('img');
+            imageElement.src = `https://api.kedufront.juniortaker.com/item/picture/${id}`;
             imageElement.classList.add('item-image');
             itemElement.appendChild(imageElement);
 
@@ -37,14 +23,6 @@ fetch('https://api.kedufront.juniortaker.com/item/')
             itemElement.appendChild(priceElement);
 
             itemsContainer.appendChild(itemElement);
-
-            // Fetch the image for the item
-            getItemImage(item.id)
-                .then(imageUrl => {
-                    // Set the image URL
-                    imageElement.src = imageUrl;
-                })
-                .catch(error => console.error('Error:', error));
         });
     })
     .catch(error => console.error('Error:', error));
