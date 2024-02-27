ada845762b5a55ab5232db2e55aea8df14a69495
ymehili <youssefmehili@gmail.com>
2024-02-27 21:57:57 +0100

    Add font styles and item details to product page

--- product/product.js
+++ product/product.js
@@ -1,2 +1,34 @@
 let params = new URLSearchParams(window.location.search);
 let id = params.get('id');
+
+fetch(`https://api.kedufront.juniortaker.com/item/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        const itemContainer = document.getElementById('itemContainer');
+
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('item-box');
+
+        const imageElement = document.createElement('img');
+        imageElement.src = `https://api.kedufront.juniortaker.com/item/picture/${id}`;
+        imageElement.classList.add('item-image');
+        itemElement.appendChild(imageElement);
+
+        const priceElement = document.createElement('p');
+        priceElement.textContent = data.price + ' €';
+        priceElement.classList.add('item-price');
+        itemElement.appendChild(priceElement);
+
+        const descElement = document.createElement('p');
+        descElement.textContent = data.description;
+        descElement.classList.add('item-desc');
+        itemElement.appendChild(descElement);
+
+        const nameElement = document.createElement('p');
+        nameElement.textContent = data.name.toUpperCase();
+        nameElement.classList.add('item-name');
+        itemElement.appendChild(nameElement);
+
+        itemContainer.appendChild(itemElement);
+    })
+    .catch(error => console.error('Error:', error));
