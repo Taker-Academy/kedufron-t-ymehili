8bb3b13efbc7ce644f0fc9d6c5735f2ced99aa33
ymehili <youssefmehili@gmail.com>
2024-02-29 12:53:29 +0100

    Refactor product.css and product.js

--- product/product.js
+++ product/product.js
@@ -11,21 +11,23 @@ fetch(`https://api.kedufront.juniortaker.com/item/${id}`)
         imageElement.classList.add('item-image');
         itemElement.appendChild(imageElement);
 
-        const priceElement = document.createElement('p');
-        priceElement.textContent = data.item.price + ' €';
-        priceElement.classList.add('item-price');
-        itemElement.appendChild(priceElement);
-
-        const descElement = document.createElement('p');
-        descElement.textContent = data.item.description;
-        descElement.classList.add('item-desc');
-        itemElement.appendChild(descElement);
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('text-container');
+        itemElement.appendChild(textContainer);
 
         const nameElement = document.createElement('p');
         nameElement.textContent = data.item.name.toUpperCase();
         nameElement.classList.add('item-name');
-        itemElement.appendChild(nameElement);
+        textContainer.appendChild(nameElement);
 
-        itemContainer.appendChild(itemElement);
+        const priceElement = document.createElement('p');
+        priceElement.textContent = data.item.price + ' €';
+        priceElement.classList.add('item-price');
+        textContainer.appendChild(priceElement);
+
+        const descElement = document.createElement('p');
+        descElement.textContent = data.item.description;
+        descElement.classList.add('item-desc');
+        textContainer.appendChild(descElement);
     })
     .catch(error => console.error('Error:', error));
